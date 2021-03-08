ea8cb79730214082a7cd317e1e945c6f2d00a032
wuweiss <wuweiss@protonmail.ch>
2021-03-08 16:00:05 +0100

    Add task creation route

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+"""task server"""
+from typing import Optional
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Task(BaseModel):
+    id: Optional[int] = None
+    tag: list = []
+    name: str
+    text: str
+
+
+taskStore = [Task]
+
+
 @app.get("/")
 async def root():
+    """This is the main route which does magic"""
     return {"message": "Hello World"}
+
+
+# request:
+# curlie --location --request POST 'http://localhost:8000/task' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{"name":"task one", "tag": ["tag1", "tag2"], "text": "mytext"}'
+#
+# response:
+# {"id": 1}
+@app.post("/task", status_code=201)
+async def creat_task(task: Task):
+    """
+    This function receives a task and adds the task to the task list
+    """
+    task.id = len(taskStore) + 1
+    taskStore.append(task)
+    return {"id": task.id}
