847aad5ebdfcc0be01b04c819aa0eee320ef5f3f
wuweiss <wuweiss@protonmail.ch>
2021-03-08 11:52:08 +0100

    Add dev dependies for nix

---
+++ fastapi/shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python38Packages.python
+    pkgs.python38Packages.python-language-server
+    pkgs.python38Packages.jedi
+    pkgs.python38Packages.rope
+    pkgs.python38Packages.pyflakes
+    pkgs.python38Packages.mccabe
+    pkgs.python38Packages.pycodestyle
+    pkgs.python38Packages.pydocstyle
+    pkgs.python38Packages.pylint
+    pkgs.python38Packages.flake8
+    pkgs.python38Packages.autopep8
+
+
+   # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+
+  shellHook = ''
+    echo starting...
+  '';
+  PORT = 5001;
+}
