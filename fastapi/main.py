d1ec831e051a69c7c2c15b6be33f42bdcd27d691
wuweiss <wuweiss@protonmail.ch>
2021-03-20 00:02:04 +0100

    Add get task by id

--- fastapi/main.py
+++ fastapi/main.py
@@ -22,11 +22,20 @@ async def create_task(task: Task):
     """
     This function receives a task and adds the task to the task list
     """
-    task.id = len(taskStore) + 1
+    task.id = len(taskStore)
     taskStore[task.id] = task
     return {"id": task.id}
 
 
 @app.get("/task")
 async def get_tasks():
-    return {"tasks": taskStore}
+    return {"task": taskStore}
+
+
+@app.get("/task/{taskid}")
+async def get_task(taskid: int):
+    task = taskStore.get(taskid)
+    if task == None:
+        return {}
+    else:
+        return task
